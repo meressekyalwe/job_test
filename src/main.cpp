49edb473b2dc578101dafb59a9e513ca6cb7fb32
meresse <meressekyalwe@live.com>
2023-09-17 02:02:54 +0300

    code reads files

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-#include <opencv2/core/version.hpp>
+#include <opencv2/opencv.hpp>
+
 
 int main()
 {
- 
- std::cout << "OpenCV version: " << CV_VERSION << std::endl;
-  return 0;
+    std::string imagePath = RESOURCE_DIR "jpeg/sample001.jpeg";
+
+    cv::Mat image = cv::imread(imagePath);
+
+    if (image.empty()) {
+        std::cerr << "Impossible de charger l'image." << std::endl;
+        return -1;
+    }
+
+    image = cv::imread(imagePath);
+
+    std::cout << "Largeur de l'image : " << image.cols << " pixels" << std::endl;
+    std::cout << "Hauteur de l'image : " << image.rows << " pixels" << std::endl;
+
+    cv::waitKey(0);
+
+   return 0;
 }
