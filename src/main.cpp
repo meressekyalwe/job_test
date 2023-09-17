eb065409377b9570f0ae14d63f5a7d3f3e98d1a7
meresse <meressekyalwe@live.com>
2023-09-17 03:42:13 +0300

    mip

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,36 @@
+#define OPENCV_IO_ENABLE_OPENEXR 1
+
 #include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
 
 
 int main()
 {
-    std::string imagePath = RESOURCE_DIR "jpeg/sample001.jpeg";
+    std::string image_source_Path = RESOURCE_DIR "png/sample001.png";
 
-    cv::Mat image = cv::imread(imagePath);
+    cv::Mat image_source = cv::imread(image_source_Path);
 
-    if (image.empty()) {
+    if (image_source.empty()) 
+    {
         std::cerr << "Impossible de charger l'image." << std::endl;
         return -1;
     }
 
-    image = cv::imread(imagePath);
+    std::vector<cv::Mat> mip_level;
+    mip_level.push_back(image_source.clone());
 
-    std::cout << "Largeur de l'image : " << image.cols << " pixels" << std::endl;
-    std::cout << "Hauteur de l'image : " << image.rows << " pixels" << std::endl;
+    while (image_source.cols > 2 && image_source.rows > 2) 
+    {
+        cv::resize(image_source, image_source, cv::Size(), 0.5, 0.5, cv::INTER_LINEAR);       
+        mip_level.push_back(image_source.clone());
+    }
 
-    cv::waitKey(0);
+    for (size_t i = 0; i < mip_level.size(); ++i) 
+    {
+        std::string image_output_Path = OUTPUT_DIR "mip_level_" + std::to_string(i) + ".png";
+        cv::imwrite(image_output_Path, mip_level[i]);
+    }
 
    return 0;
 }
